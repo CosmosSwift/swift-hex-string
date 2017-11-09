86af0d4f44e10273203c5719a2c021006e72555c
Alex Tran Qui <alex@katalysis.io>
2017-11-09 19:09:43 +0100

    add missing products

--- Package.swift
+++ Package.swift
@@ -4,6 +4,9 @@ import PackageDescription
 
 let package = Package(
     name: "HexString",
+    products: [
+      .library(name: "HexString", targets: ["HexString"])
+    ],
     targets: [
       .target(name: "HexString", path: ".", sources: ["Sources"])
     ]
